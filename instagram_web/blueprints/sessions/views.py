139c4f5266e0dfec45026e4a8bd17ffe0040d47a
unknown <110853MY@gmail.com>
2020-02-26 18:08:55 +0800

    login form

--- instagram_web/blueprints/sessions/views.py
+++ instagram_web/blueprints/sessions/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash
 from models.user import User
 
 sessions_blueprint = Blueprint('sessions',
@@ -10,3 +10,21 @@ sessions_blueprint = Blueprint('sessions',
 @sessions_blueprint.route('/new', methods=['GET'])
 def new():
     return render_template('sessions/new.html')
+
+
+@sessions_blueprint.route('/', methods=['POST'])
+def login():
+    password_to_check = request.form['password']
+    hashed_password = User.password
+    result = check_password_hash(hashed_password, password_to_check)
+
+    username = request.form.get('username')
+
+    try:
+        if username == 'username' and result == 'password':
+            flash('Login Successfull', 'success')
+            return redirect(url_for('home'))
+
+    except:
+        flash('Login Failed', 'danger')
+        return redirect(url_for('sessions.new'))
